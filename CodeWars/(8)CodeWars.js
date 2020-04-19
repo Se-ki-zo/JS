49e99253d6162cebaa0c93aa11723f907d0c51c0
Urfill <53227792+Urfill@users.noreply.github.com>
2020-04-19 14:32:41 +0300

    js 19.04.20

--- CodeWars/(8)CodeWars.js
+++ CodeWars/(8)CodeWars.js
@@ -1,1 +1,31 @@
-allalalalal
+/*
+
+The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
+
+Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+
+Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue?
+
+Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment. Otherwise return NO.
+
+*/
+
+// NOT MY
+
+function tickets(peopleInLine){
+  let [c25,c50,c100] = [0,0,0];
+  for(let v of peopleInLine) {
+    if(v===25) c25++;
+    if(v===50) {c50++; c25--;}
+    if(v===100) {c25--; c50>0?c50--:c25-=2;}
+    if(c25<0||c50<0) return 'NO'
+  }
+  return 'YES'
+}
+
+tickets([25, 25, 50]); // YES 
+tickets([25, 100]); // NO
+tickets([25, 25, 50, 50, 100]); // NO
+tickets([25, 100]); // NO
+tickets([25, 25, 50, 50]); // YES
+tickets([100, 25, 25, 25, 25]); // NO
--- CodeWars/(9)CodeWars.js
+++ CodeWars/(9)CodeWars.js
@@ -1,1 +1,28 @@
-lalalal
+/* 
+
+You are going to be given a word. Your job is to return the middle character of the word. 
+
+If the word's length is odd, return the middle character.
+
+If the word's length is even, return the middle 2 characters.
+
+*/
+
+function getMiddle(str) {
+    let len;
+    let res;
+
+    if (str.length % 2 == 0) {
+        len = (str.length / 2) - 1;
+        res = str.charAt(len) + str.charAt(len + 1);
+    } else {
+        len = Math.floor(str.length / 2);
+        res = str.charAt(len);
+    }
+
+    return res;
+}
+
+getMiddle("test"); // es
+getMiddle("testing"); // t
+getMiddle("middle"); // dd
