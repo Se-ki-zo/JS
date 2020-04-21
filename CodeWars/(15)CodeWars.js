ec82402fb8598f2f76d3d58a546cd52a38b73916
Urfill <53227792+Urfill@users.noreply.github.com>
2020-04-21 22:44:15 +0300

    js 21.04.20

---
+++ CodeWars/(15)CodeWars.js
@@ -0,0 +1,36 @@
+/*
+
+What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+'abba' & 'baab' == true
+
+'abba' & 'bbaa' == true
+
+'abba' & 'abbba' == false
+
+'abba' & 'abca' == false
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy', 'lacer']) => []
+
+*/
+
+function anagrams(str, arr) {
+    let res;
+    str = str.split('').sort().join('');
+
+    for (let i = 0; i < arr.length; i++) {
+        let a = arr[i].split('').sort().join('');
+
+        if (str == a) {
+            console.log(`true (${arr[i]})`);
+        }
+    }
+    console.log(false);
+}
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']); // ['carer', 'racer']
---
+++ CodeWars/(16)CodeWars.js
@@ -0,0 +1,34 @@
+/*
+
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. 
+
+We want to create the text that should be displayed next to such an item.
+
+Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. 
+
+It must return the display text as shown in the examples.
+
+*/
+
+function likes(names) {
+    let res;
+
+    if (names.length == 0) {
+        res = 'no one likes this'
+    } else if (names.length == 1) {
+        res = `${names[0]} likes this`;
+    } else if (names.length == 2) {
+        res = `${names[0]} and ${names[1]} like this`;
+    } else if (names.length == 3) {
+        res = `${names[0]}, ${names[1]} and ${names[2]} like this`
+    } else {
+        res = `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+    }
+    return res;
+}
+
+// likes ([]); // "no one likes this"
+// likes (["Peter"]); // "Peter likes this"
+//likes (["Jacob", "Alex"]); // "Jacob and Alex like this"
+//likes (["Max", "John", "Mark"]); // "Max, John and Mark like this"
+likes(["Alex", "Jacob", "Mark", "Max"]); // "Alex, Jacob and 2 others like this"
