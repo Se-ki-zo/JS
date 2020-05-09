58455e0182c07fa1e030ee9e7e6aad7c19342ca5
Urfill <53227792+Urfill@users.noreply.github.com>
2020-05-09 12:37:31 +0300

    9.05.20

--- TheCalculator!/calc.js
+++ TheCalculator!/calc.js
@@ -2,6 +2,8 @@ let firstNum = '';
 let secondNum = '';
 let result;
 let symbol = null; // || + || - || / || * ||
+let commaFirst = 0;
+let commaSecond = 0;
 
 const displayFirstNum = document.getElementById('displayFirstNum');
 const displaySecondNum = document.getElementById('displaySecondNum');
@@ -12,85 +14,94 @@ const maxNumLength = 10;
 
 function push(num) {
 
-    if (symbol == null && firstNum.length < maxNumLength) { // if (num[0] !=0)
+    if (isFinite(result) == true) { // Очистка экрана и переменных в случае вызова цифры, после равно
+        fullClear();
+        firstNum += num;
+        return displayFirstNum.innerHTML = firstNum;
+    }
+
+    if (symbol == null && firstNum.length < maxNumLength) { // Запись первого числа и вывод его на экран
         firstNum += num;
         displayFirstNum.innerHTML = firstNum;
-    } else if (secondNum.length < maxNumLength) {
+    } else if (secondNum.length < maxNumLength) { // Запись второго числа и вывод его на экран, если знак был применен
         secondNum += num;
         displaySecondNum.innerHTML = secondNum;
     }
-    // clear(2);
 
 }
 
 
 function action(sym) {
 
-    displayAction.innerHTML = sym;
-    symbol = sym;
+    if (firstNum != '') { // Проверка на 1ое число
 
-    if (isFinite(result) == true) {
-        if (Number.isInteger(result) == true) {
-            firstNum = result;
-        } else {
-            firstNum = (result).toFixed(3);
+        displayAction.innerHTML = sym; // Добавление знака на экран и запись в переменную
+        symbol = sym;
+
+        if (isFinite(result) == true) { // Очистка экрана и переменных после повторного запуска знака 
+            if (Number.isInteger(result) == true) {
+                firstNum = result;
+            } else {
+                firstNum = (result);
+            }
+            result = undefined;
+            secondNum = '';
+            displayFirstNum.innerHTML = firstNum;
+            displaySecondNum.innerHTML = '';
+            displayEq.innerHTML = '';
         }
-
-        displayFirstNum.innerHTML = firstNum;
-        secondNum = '';
-        displaySecondNum.innerHTML = '';
-        displayEq.innerHTML = '';
     }
-    // clear(1);
+}
+
+function comma() {
+    
+    
+}
+
+function plusMinus() {
+
 }
 
 
 function sum() {
 
-    if (secondNum == '') {
-        secondNum = firstNum;
-        displaySecondNum.innerHTML = secondNum;
-    }
+    if (symbol != null) { // Проверка на первое число и знак - иначе Err в консоли
 
-    if (symbol === '+') {
-        result = parseFloat(firstNum) + parseFloat(secondNum);
-    } else if (symbol === '-') {
-        result = parseFloat(firstNum) - parseFloat(secondNum);
-    } else if (symbol === '/') {
-        result = parseFloat(firstNum) / parseFloat(secondNum);
-    } else if (symbol === 'x') {
-        result = parseFloat(firstNum) * parseFloat(secondNum);
-    }
+        if (secondNum == '' && symbol != null) { // Запуск выполнения действия если нет 2ого числа
+            secondNum = firstNum;
+            displaySecondNum.innerHTML = secondNum;
+        }
 
-    if (Number.isInteger(result) == true) {
-        displayEq.innerHTML = `= ${result}`;
-    } else {
-        displayEq.innerHTML = `=&nbsp${(result).toFixed(3)}`;
+        if (symbol === '+') {
+            result = parseFloat(firstNum) + parseFloat(secondNum);
+        } else if (symbol === '-') {
+            result = parseFloat(firstNum) - parseFloat(secondNum);
+        } else if (symbol === '/') {
+            result = parseFloat(firstNum) / parseFloat(secondNum);
+        } else if (symbol === 'x') {
+            result = parseFloat(firstNum) * parseFloat(secondNum);
+        }
+
+        if (result == Infinity) { // Деление на 0
+            alert('num / 0 = Err!');
+            return fullClear();
+        }
+
+        if (Number.isInteger(result) == true) {
+            displayEq.innerHTML = `= ${result}`;
+        } else {
+            displayEq.innerHTML = `= ${(result)}`;
+        }
     }
 }
 
-
-// function clear(ver) { //Err
-
-//     if (isFinite(result) == true && ver == 1) {
-//         if (Number.isInteger(result) == true) {
-//             firstNum = result;
-//         } else {
-//             firstNum = (result).toFixed(3);
-//         }
-
-//         displayFirstNum.innerHTML = firstNum;
-//         secondNum = '';
-//         displaySecondNum.innerHTML = '';
-//         displayEq.innerHTML = '';
-
-//     } else if (isFinite(result) == true && ver == 2) {
-//         displayFirstNum.innerHTML = firstNum;
-//         secondNum = '';
-//         symbol = null;
-//         displayAction.innerHTML = '';
-//         displaySecondNum.innerHTML = '';
-//         displayEq.innerHTML = '';
-
-//     }
-// }
+function fullClear() { // Очистка экрана и переменных
+    firstNum = '';
+    secondNum = '';
+    symbol = null;
+    result = undefined;
+    displayFirstNum.innerHTML = '';
+    displaySecondNum.innerHTML = '';
+    displayAction.innerHTML = '';
+    displayEq.innerHTML = '';
+}
